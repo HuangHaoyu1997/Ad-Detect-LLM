0c4e39e959244fe92aec74098e830c6e0849647d
HuangHaoyu1997 <446700679@qq.com>
2025-09-05 19:18:14 +0800

    major update

--- README.md
+++ README.md
@@ -14,8 +14,37 @@ conda activate ad-detect
 pip install -r requirements.txt
 ``` 
 
+使用[ms-swift微调框架](https://github.com/modelscope/ms-swift/)。
+```bash
+pip install ms-swift -U
+
+# please use python=3.10, cuda12.*
+# sh requirements/install_all.sh
+pip install "sglang[all]<0.5" -U
+pip install "vllm>=0.5.1" "transformers<4.56" "trl<0.21" -U
+pip install "lmdeploy>=0.5" -U
+pip install autoawq -U --no-deps
+pip install auto_gptq optimum bitsandbytes "gradio<5.33" -U
+pip install git+https://github.com/modelscope/ms-swift.git
+pip install timm -U
+pip install "deepspeed" -U
+pip install qwen_vl_utils qwen_omni_utils decord librosa icecream soundfile -U
+pip install liger_kernel nvitop pre-commit math_verify py-spy -U
+# flash-attn: https://github.com/Dao-AILab/flash-attention/releases
+```
 
 ## 运行程序
 
+ms-swift命令行参数解释：
+https://github.com/modelscope/ms-swift/blob/main/docs/source/Instruction/%E5%91%BD%E4%BB%A4%E8%A1%8C%E5%8F%82%E6%95%B0.md
+
+### Merge LoRA
+```bash
+# Since `output/vx-xxx/checkpoint-xxx` is trained by swift and contains an `args.json` file,
+# there is no need to explicitly set `--model`, `--system`, etc., as they will be automatically read.
+swift export \
+    --adapters output/vx-xxx/checkpoint-xxx \
+    --merge_lora true
+```
 
 
--- src/inference.py
+++ src/inference.py
@@ -1,26 +1,31 @@
 import json
+import time
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-
-
-if __name__ == "__main__":
-    # 读取./sft_dataset/test.json文件
-    with open('./sft_dataset/test.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-
-
-    model_name = "/mnt/e/LLM/Qwen3-0.6B"
-    # model_name = "/mnt/e/LLM/Qwen3-4B-Instruct-2507"
-
+def get_model_and_tokenizer(model_name):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         torch_dtype="auto",
         device_map="auto"
     )
+    return model, tokenizer
+
+if __name__ == "__main__":
+    # 读取./sft_dataset/test.json文件
+    with open('/home/hhy/ASR/Ad-Detect-LLM/sft_data/test.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+
+    # model_name = "/mnt/e/LLM/Qwen3-0.6B"
+    model_name = "/home/hhy/ASR/Ad-Detect-LLM/output/v1-20250905-155935/checkpoint-100-merged/"
+    # model_name = "/mnt/e/LLM/Qwen3-4B-Instruct-2507"
+
+    model, tokenizer = get_model_and_tokenizer(model_name)
 
     # 跑一遍测试集
     count = 0
+    start_time = time.time()
     for i in range(len(data)):
         
         text = tokenizer.apply_chat_template(
@@ -30,12 +35,8 @@ if __name__ == "__main__":
             enable_thinking=False,
         )
         model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-        generated_ids = model.generate(
-            **model_inputs,
-            max_new_tokens=32768
-            )
+        generated_ids = model.generate(**model_inputs, max_new_tokens=32768)
         output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
-
 
         try:
             # rindex finding 151668 (</think>)
@@ -45,7 +46,8 @@ if __name__ == "__main__":
 
         thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
         content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
-        print(i, content)
+        
+        print(i, content, data[i]['messages'][2]['content'])
         # 如果判断正确
         if '是' in content and '是' in data[i]['messages'][2]['content']:
             count += 1
@@ -54,7 +56,8 @@ if __name__ == "__main__":
             count += 1
             print('yes')
         if i % 100 == 0:
-            print('accuracy:', count/len(data))
+            print('time:', (time.time() - start_time)/(i+1))
+            print('accuracy:', count/(i+1))
         # print("thinking content:", thinking_content)
         
     print("count:", count, 'accuracy:', count/len(data))
